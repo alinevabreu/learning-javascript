779214f262551c41e4aecaf3c76c248c3aad63b1
Aline Abreu <aline.vabreu@gmail.com>
2020-12-08 09:53:01 +0100

    Exercicio 10

---
+++ curso-gustavo/ex008.js
@@ -0,0 +1,7 @@
+var vel = 72.2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { //Condicao simples!
+    console.log('Voce ultrrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
